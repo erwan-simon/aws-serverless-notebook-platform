1afc7f200d2cf3ffd4f68e7378f6006912395926
erwan-simon <erwan.simon@devoteam.com>
2026-04-12 21:44:36 +0200

    fix: grant dynamodb:UpdateItem to upload_notebook lambda for schedule persistence

--- iac/backend_upload_notebook_lambda.tf
+++ iac/backend_upload_notebook_lambda.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "upload_notebook_lambda" {
     resources = ["${aws_s3_bucket.notebooks.arn}/${local.notebooks_s3_prefix}*"]
   }
   statement {
-    actions   = ["dynamodb:PutItem", "dynamodb:GetItem"]
+    actions   = ["dynamodb:PutItem", "dynamodb:GetItem", "dynamodb:UpdateItem"]
     resources = [aws_dynamodb_table.notebooks.arn]
   }
   statement {
